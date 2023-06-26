f02c63dd9ac2bed3bb4363c293752b4d38d39b61
unknown <maryrodri3210@gmail.com>
2023-06-26 14:27:28 -0700

    Added port to server.js

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ app.use(cors());
 
 const chocolistController = require('./controllers/chocolist-controller')
 
+require('dotenv').config()
+require('./config/db.connection')
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use('/posts', chocolistController)
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Listening on port ${ PORT}`));
